10373ed5c94db9ef0649648b30b13c59812858f2
Ramon Recuero <ramon@oly.ai>
2018-10-05 23:18:01 -0700

    Post backend placeholder

---
+++ backend/api/posts.js
@@ -0,0 +1,14 @@
+// import { config } from 'config';
+
+function writePost(req, res) {
+  // Get user address it will be in the payload
+  // 0. Write file to IPFS (maybe change the owner to the address)
+  // 1. Create Post by calling PostFactory
+  // 2. Transfer the post owner to the eth address
+  res.status(200).send({ result: {} });
+}
+
+
+module.exports = (app) => {
+  app.post('/api/post', writePost);
+};
